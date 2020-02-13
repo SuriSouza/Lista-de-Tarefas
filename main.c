6e736d46f05c555f03b8ff1d57b642a96898f7fe
LEGEND-source <alexsandermarx0@gmail.com>
2020-02-13 18:01:14 -0300

    TESTA AEEEEEEEEE

--- main.c
+++ main.c
@@ -9,46 +9,46 @@ int main(int argc, char const *argv[])
 	char resposta = 's';
 	char nomeaux[20];
 	int opcao;
-	TListadeListas *listaDeListas;
+	TListadeListas listaDeListas;
 	TListadeListas *listaAux;
-	FLVazia(&listaDeListas->lista);
-	listaDeListas->prox = NULL;
+	FLVazia(&listaDeListas.lista);
+	listaDeListas.prox = NULL;
 	do{
 		MenuInicial();
 		scanf("%d", &opcao);
 
 		switch (opcao) {
 			case 1:
-			criaLista(listaDeListas);
+			criaLista(&listaDeListas);
 				break;
 
 			case 2:
-				ImprimirListas(listaDeListas);
+				ImprimirListas(&listaDeListas);
 			break;
 
 			case 3:
-				ImprimirListas(listaDeListas);
+				ImprimirListas(&listaDeListas);
 				printf("Digite o nome da lista que deseja remover: \n");
 				__fpurge(stdin);
 				fgets(nomeaux,20,stdin);
-				listaAux = buscaListas(nomeaux, listaDeListas);
+				listaAux = buscaListas(nomeaux, &listaDeListas);
 					if (listaAux != NULL){
-					if (listaDeListas->prox != NULL && strcmp(listaDeListas->prox->lista.nome, nomeaux) == 0)
-					removerLista(listaDeListas);
+					if (listaDeListas.prox != NULL && strcmp(listaDeListas.prox->lista.nome, nomeaux) == 0)
+					removerLista(&listaDeListas);
 					else removerLista(listaAux);
 				}
 				else printf(">>>>> NÃO ENCONTRADO <<<<<\n");
 			break;
 
 			case 4:
-			ImprimirListas(listaDeListas);
+			ImprimirListas(&listaDeListas);
 			printf("Digite o nome da lista que deseja modificar: \n");
 			__fpurge(stdin);
 			fgets(nomeaux,20,stdin);
-			listaAux = buscaListas(nomeaux, listaDeListas);
+			listaAux = buscaListas(nomeaux, &listaDeListas);
 				if (listaAux != NULL){
-				if (listaDeListas->prox != NULL && strcmp(listaDeListas->prox->lista.nome, nomeaux) == 0)
-				MenuOpcao03(&listaDeListas->prox->lista);
+				if (listaDeListas.prox != NULL && strcmp(listaDeListas.prox->lista.nome, nomeaux) == 0)
+				MenuOpcao03(&listaDeListas.prox->lista);
 				else MenuOpcao03(&listaAux->prox->lista);
 			}
 			else printf(">>>>> NÃO ENCONTRADO <<<<<\n");
